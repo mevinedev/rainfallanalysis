2e439e9870b2a12733a6e0301bd2629f3ffda501
mevinedev <72660182+mevinedev@users.noreply.github.com>
2020-10-30 10:43:06 +0300

    Update rainfallanalysis.R

--- rainfallanalysis.R
+++ rainfallanalysis.R
@@ -1,4 +1,4 @@
-rainfall <- c(799,1174.8,865.1,1334.6,635.4,918.5,685.5,998.6,784.2,985,882.8,1071)
+rainfall <- c(899.7,1174.8,865.1,1334.6,735.4,918.5,785.5,998.6,784.2,985,882.8,1071)
 ts(rainfall,start = c(2012,1),frequency = 12) -> rainfall.timeseries
 print(rainfall.timeseries)
 plot(rainfall.timeseries)
